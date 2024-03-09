6a0748ada6ba74703e79c8856a841924400694c2
Boring Task AI <boringtaskai@gmail.com>
2024-03-09 18:20:19 +0700

    Change to boringtaskai repo

--- scripts/init_local_dev.sh
+++ scripts/init_local_dev.sh
@@ -4,22 +4,24 @@
 #
 # Athor: Felix Lorenz
 # License: WAT
+#
+# Change to boringtask ai local development: Yusak Setiawan
 
 check_ssh_access() {
     # Attempt to connect to GitHub via SSH and check for successful authentication
-    ssh -T git@github.com 2>&1 | grep -q "successfully authenticated"
+    ssh -T git@github-boringtaskai 2>&1 | grep -q "successfully authenticated"
     return $?
 }
 
 REPOSITORIES=(
-  "okio-ai/nendo-web"
-  "okio-ai/nendo-server"
+  "boringtaskai/nendo-web"
+  "boringtaskai/nendo-server"
 )
 
 # clone all main repos
 if check_ssh_access; then
   for repo in "${REPOSITORIES[@]}"; do
-    CLONE_URL="git@github.com:${repo}.git"
+    CLONE_URL="git@github-boringtaskai:${repo}.git"
     git clone "${CLONE_URL}" "../repo/$(basename "${repo}")"
   done
 else
